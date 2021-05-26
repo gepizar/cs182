ad55c35578deed67e5c63d63226230c1d713545f
gepizar <gepizarro@uc.cl>
2021-05-26 19:06:14 -0400

    feat: Adam
    Implement optimization algorithm

--- hw1/deeplearning/optim.py
+++ hw1/deeplearning/optim.py
@@ -140,7 +140,19 @@ def adam(x, dx, config=None):
     # the next_x variable. Don't forget to update the m, v, and t variables     #
     # stored in config and to use the epsilon scalar to avoid dividing by zero. #
     #############################################################################
-    pass
+    beta1 = config['beta1']
+    beta2 = config['beta2']
+    lr = config['learning_rate']
+    eps = config['epsilon']
+
+    config['m'] = (1 - beta1) * dx + beta1 * config['m']
+    config['v'] = (1 - beta2) * dx ** 2 + beta2 * config['v']
+    config['t'] += 1
+
+    mk = config['m'] / (1 - beta1 ** config['t'])
+    vk = config['v'] / (1 - beta2 ** config['t'])
+
+    next_x = x - lr * mk / (np.sqrt(vk) + eps)
     #############################################################################
     #                             END OF YOUR CODE                              #
     #############################################################################
