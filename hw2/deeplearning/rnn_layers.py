00b2bdce0df8fc086f8078b522fa30d6667262f4
gepizar <gepizarro@uc.cl>
2021-06-30 21:25:00 -0400

    feat: Word Embedding
    Implement Forward and Backprop.

--- hw2/deeplearning/rnn_layers.py
+++ hw2/deeplearning/rnn_layers.py
@@ -180,7 +180,8 @@ def word_embedding_forward(x, W):
     #                                                                            #
     # HINT: This can be done in one line using NumPy's array indexing.           #
     ##############################################################################
-    pass
+    out = W[x]
+    cache = x, W
     ##############################################################################
     #                               END OF YOUR CODE                             #
     ##############################################################################
@@ -209,7 +210,9 @@ def word_embedding_backward(dout, cache):
     # Note that Words can appear more than once in a sequence.                   #
     # HINT: Look up the function np.add.at                                       #
     ##############################################################################
-    pass
+    x, W = cache
+    dW = np.zeros_like(W)
+    np.add.at(dW, x, dout)
     ##############################################################################
     #                               END OF YOUR CODE                             #
     ##############################################################################
