d32d43a861d470df37a6a5c60f6816c71c54df57
gepizar <gepizarro@uc.cl>
2021-07-09 22:34:18 -0400

    feat: LanguageModel
    Implement PyTorch language model. Init and forward.

--- hw3/language_model.py
+++ hw3/language_model.py
@@ -11,22 +11,22 @@ class LanguageModel(nn.Module):
         # Create an embedding layer of shape [vocab_size, rnn_size]
         # Use nn.Embedding
         # That will map each word in our vocab into a vector of rnn_size size.
-        self.embedding = your_code
+        self.embedding = nn.Embedding(vocab_size, rnn_size)
 
         # Create an LSTM layer of rnn_size size. Use any features you wish.
         # We will be using batch_first convention
-        self.lstm = your_code
+        self.lstm = nn.LSTM(rnn_size, rnn_size, num_layers=num_layers,
+            batch_first=True)
         # LSTM layer does not add dropout to the last hidden output.
         # Add this if you wish.
-#         self.dropout = your_code
+        self.dropout = nn.Dropout(dropout)
         # Use a dense layer to project the outputs of the RNN cell into logits of
         # the size of vocabulary (vocab_size).
-        self.output =       
+        self.output = nn.Linear(rnn_size, vocab_size) 
         
     def forward(self,x):
-        embeds = your_code
-        lstm_out, _ = your_code
-#         lstm_drop = your_code
-#         logits = your_code
-        logits = your_code
+        embeds = self.embedding(x)
+        lstm_out, _ = self.lstm(embeds)
+        lstm_drop = self.dropout(lstm_out)
+        logits = self.output(lstm_drop)
         return logits
